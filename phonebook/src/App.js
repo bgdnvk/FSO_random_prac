e92407c71585ffc49c02c8963fb87209f1b978d0
bgdnvk <bnovykov@gmail.com>
2021-01-29 17:05:53 +0100

    db

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,22 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Entry from './components/Entry'
 import FilterPhones from './components/FilterPhones'
 import PersonForm from './components/PersonForm'
 import Title from './components/Title'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterName, setfilterName ] = useState('')
   const [ displayPeople, setDisplayPeople ] = useState(persons)
 
+  const hook = () => {
+    console.log('inside hook');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        setPersons(res.data)
+        setDisplayPeople(res.data)
+      })
+  }
 
+  useEffect(hook, [])
 
   return (
     <div>
